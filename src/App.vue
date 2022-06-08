7cf8d3dee933be663ae10b07d3ca8c0486ed7900
Buzz88-Dev <omarbuzzoni88@gmail.com>
2022-06-08 15:20:11 +0200

    Aggiornamento Header

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <DcHeader />
+    <DcMain />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DcHeader from './components/Header.vue';
+import DcMain from './components/Main.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DcHeader,
+    DcMain,
   }
 }
 </script>
@@ -20,8 +23,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="header">
+    <nav class="navbar">
+
+      <div class="image">
+        <img src="../assets/dc-logo.png">
+      </div>
+
+
+      <ul>
+          <li>
+              <a href="#">CHARACTERS</a>
+          </li>
+          <li>
+              <a href="#">COMICS</a>
+          </li>
+          <li>
+              <a href="#">MOVIES</a>
+          </li>
+          <li>
+              <a href="#">TV</a>
+          </li>
+          <li>
+              <a href="#">GAMES</a>
+          </li>
+          <li>
+              <a href="#">COLLECTIBLES</a>
+          </li>
+          <li>
+              <a href="#">VIDEOS</a>
+          </li>
+          <li>
+              <a href="#">FANS</a>
+          </li>
+          <li>
+              <a href="#">NEWS</a>
+          </li>
+          <li>
+              <a href="#">SHOP</a>
+          </li>
+      </ul>
+    </nav>  
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DcHeader',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+  .header {
+    width: 80%;
+    margin: auto;
+  }
+  .navbar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    // Debug
+    background-color: red;
+  }
+
+  .image img {
+    width: 60%;
+    height: auto;
+    padding: 15px 0px;
+  }
+
+  ul {
+    list-style-type: none;
+
+    li {
+      display: inline-block;
+      font-size: 11px;
+
+      a {
+        text-decoration: none;
+        color: black;
+        margin-left: 20px;
+
+        &:hover {
+          color: rgb(28, 133, 198);
+        }
+      }
+    }
+  }
+</style>
